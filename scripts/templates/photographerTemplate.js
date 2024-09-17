76b9284dc83be3733f0fcec99414e03d996cc562
Fred jeune DEV en devenir <fredstessy@outlook.fr>
2024-09-17 15:49:57 +0200

    first commit home page

---
+++ scripts/templates/photographerTemplate.js
@@ -0,0 +1,20 @@
+export function photographerTemplate(data) {
+  const { name, portrait, city, country, tagline, price } = data;
+
+  const picture = `assets/photographers/${portrait}`;	
+
+  function getUserCardDOM() {
+      const article = document.createElement('article');
+
+      article.innerHTML = `	
+        <img src="${picture}" alt="Photo de ${name}">
+        <h2>${name}</h2>
+        <p class="location">${city}, ${country}</p>
+        <p>${tagline}</p>
+        <p class="prix">${price}€/jour</p>
+      `;
+      return article;
+  }
+
+  return { name, picture, getUserCardDOM };
+}
