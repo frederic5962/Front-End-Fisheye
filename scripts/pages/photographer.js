b1e673510a64fa0abddbc21f4ad077797d6d02fd
Fred jeune DEV en devenir <fredstessy@outlook.fr>
2024-10-24 13:40:32 +0200

    modification de la Factory pour la page photographe

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,37 +1,27 @@
 import { getPhotographers } from '../utils/fetchData.js';
-
+import { photographerFactory } from '../factory/photographer.js';
 
 async function displayPhotographerData() {
-  const photographers = await getPhotographers(); // Récupère tous les photographes
-  const photographerId = getPhotographerIdFromUrl(); // Récupère l'ID du photographe depuis l'URL
-  const photographer = photographers.find(p => p.id === photographerId); // Trouve le photographe correspondant
+  const photographers = await getPhotographers();
+  const photographerId = getPhotographerIdFromUrl();
+  const photographerData = photographers.find(p => p.id === photographerId);
 
-  if (!photographer) {
+  if (!photographerData) {
     console.error('Photographe non trouvé');
     return;
   }
 
-  // Création et affichage des détails du photographe
+  const photographer = photographerFactory(photographerData);
+
+  // Affichage des détails du photographe
   const photographerSection = document.querySelector('.photograph-header');
-  const imageElement = document.createElement('img');
-  imageElement.src = `assets/photographers-id-photo/${photographer.portrait}`;
-  imageElement.alt = photographer.name;
-  const nameElement = document.createElement('h1');
-  nameElement.textContent = photographer.name;
-  const taglineElement = document.createElement('p');
-  taglineElement.textContent = photographer.tagline;
-  const priceElement = document.createElement('p');
-  priceElement.textContent = `${photographer.price}€/jour`;
-
-
-  // Appel de la fonction pour ajouter les éléments au DOM
-  photographerSection.appendChild(imageElement);
-  photographerSection.appendChild(nameElement);
-  photographerSection.appendChild(taglineElement);
-  photographerSection.appendChild(priceElement);
-
-
-  
+  photographerSection.innerHTML = `
+    <img src="${photographer.portrait}" alt="${photographer.name}">
+    <h1>${photographer.name}</h1>
+    <p class="photograph-location">${photographer.location}</p>
+    <p class="photograph-tagline">${photographer.tagline}</p>
+    <p class="photograph-price">${photographer.price}€/jour</p>
+  `;
 }
 
 function getPhotographerIdFromUrl() {
