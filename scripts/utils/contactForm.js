8b14bc28df8a83028cea1ba5ccf124daa3038d11
Fred jeune DEV en devenir <fredstessy@outlook.fr>
2024-12-18 09:43:57 +0100

    modification css et formulaire de contact

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -27,20 +27,10 @@ function submitForm() {
   document.getElementById('contact_form').onsubmit = function (event) {
     event.preventDefault();
 
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-
-    const data = {
-      firstName,
-      lastName,
-      email,
-      message,
-    };
-
-    alert('Message envoyé !');
-    console.log(data);
+    const firstName = document.getElementById('firstName').value = '';
+    const lastName = document.getElementById('lastName').value = '';
+    const email = document.getElementById('email').value = '';
+    const message = document.getElementById('message').value = '';
 
     const modal = document.getElementById('contact_modal');
     modal.style.display = 'none';
